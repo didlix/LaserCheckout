e5116f7d7886ab4dc6fc6c152f20a6a275277f7b
Rachel Graves <rachel@didlix.net>
2015-02-19 15:02:59 +0000

    Add RSpec custom matchers to cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,6 @@
+require 'rspec'
+
 $LOAD_PATH << File.expand_path('../../../lib', __FILE__)
+Dir[File.dirname(__FILE__) + "/../../spec/support/**/*.rb"].each {|f| require f}
 
 require 'laser_checkout'
