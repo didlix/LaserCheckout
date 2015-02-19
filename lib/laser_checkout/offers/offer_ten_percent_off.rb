aac72f8468ab62ed8725a8b3048aba2d4bfbbaee
Rachel Graves <rachel@didlix.net>
2015-02-19 23:22:01 +0000

    Changes to ten percent off
    - Round numbers to 2 decimal places
    - Only apply to baskets over £60

--- lib/laser_checkout/offers/offer_ten_percent_off.rb
+++ lib/laser_checkout/offers/offer_ten_percent_off.rb
@@ -4,8 +4,16 @@ module LaserCheckout
     private
 
     def process!(basket)
+
+      total = 0
       basket.each do |item|
-        item.price = item.price * 0.9
+        total+= item.price
+      end
+
+      if total >= 60
+        basket.each do |item|
+          item.price = (item.price * 0.9).round(2)
+        end
       end
 
       basket
--- spec/offers/offer_ten_percent_off_spec.rb
+++ spec/offers/offer_ten_percent_off_spec.rb
@@ -14,5 +14,21 @@ module LaserCheckout
       expect(total).to eq(59.4)
     end
 
+    it "rounds to two decimal places" do
+      basket = [ Product.new("34", 66.25) ]
+      basket = offer.apply_to!(basket)
+      total = 0
+      basket.each { |item| total+= item.price }
+      expect(total).to eq(59.63)
+    end
+
+    it "does nothing when the basket is less than £60" do
+      basket = [ Product.new("34", 6) ]
+      basket = offer.apply_to!(basket)
+      total = 0
+      basket.each { |item| total+= item.price }
+      expect(total).to eq(6)
+    end
+
   end
 end
